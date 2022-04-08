465843934f62773d3e723ebdbeff8ebf27835e51
reinholz36 <dreinholz28@gmail.com>
2022-04-07 21:32:48 -0400

    added profile and friendslist

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
-  //ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
   ApolloClient,
-  //InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
   InMemoryCache,
-  //ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
   ApolloProvider,
-  //createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
   createHttpLink,
 } from '@apollo/client';
 
@@ -15,6 +11,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -28,13 +29,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container">
-        &copy;{new Date().getFullYear()} by Dani Reinholz
+        &copy;{new Date().getFullYear()} by Lernantino
       </div>
     </footer>
   );
